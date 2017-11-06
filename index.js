75ca282fb001616746b26b99100f47389085f263
Travis Illig <travis.illig@fiserv.com>
2017-11-06 10:37:51 -0800

    Added eslint support.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var swaggerjson = require('./routes/swaggerjson');
 var app = express();
 
 // Use X-Forwarded-* headers.
-app.enable("trust proxy");
+app.enable('trust proxy');
 
 // Turn off the Express header.
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
@@ -21,18 +21,18 @@ app.set('view engine', 'hbs');
 app.use(logger('short'));
 
 // Add CORS header middleware.
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	next();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
 });
 
 // Redirect people to get to the Swagger docs by default.
-app.get('/', function(req, res) {
-	res.redirect("/swagger/ui/");
+app.get('/', function (req, res) {
+  res.redirect('/swagger/ui/');
 });
-app.get('/swagger', function(req, res) {
-	res.redirect("/swagger/ui/");
+app.get('/swagger', function (req, res) {
+  res.redirect('/swagger/ui/');
 });
 
 // Get the dynamic content first, otherwise use static content.
@@ -41,21 +41,21 @@ app.use('/swagger/ui', index);
 app.use('/swagger/ui', express.static(path.join(__dirname, 'public')));
 
 // Catch 404 and forward to error handler.
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // Error handler.
-app.use(function(err, req, res, next) {
-	// Set locals, only providing error in development.
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development.
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// Render the error page.
-	res.status(err.status || 500);
-	res.render('error');
+  // Render the error page.
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var config = require('config');
 
-var info = config.get("info");
-var oauth = config.get("oauth");
+var info = config.get('info');
+var oauth = config.get('oauth');
 
-router.get('/', function(req, res, next) {
-	res.render('index', { title: info.title, oauth: oauth });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: info.title, oauth: oauth });
 });
 
 module.exports = router;
